e3351dcbaab0713edbd46888ebd41a9a7b9977c0
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-30 23:10:29 -0600

    Update test dir; As constant; format

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,17 @@
-const { expect, test } = require('@oclif/test');
-const cmd = require('../lib');
+const { expect, test } = require('@oclif/test')
+const cmd = require('../lib')
 
-const hasErrorMessage = (error) => expect(error.message).to.be.ok;
+const hasErrorMessage = (error) => expect(error.message).to.be.ok
+
+const TEST_DIR = 'mfe-dependencies-test-repo'
 
 describe('mfedeps', () => {
   test
     .stdout()
-    .do(() =>
-      cmd.run(['-d', 'mfe-deps-test-repo', '-f', 'src/**/!(*.test).js'])
-    )
+    .do(() => cmd.run(['-d', TEST_DIR, '-f', 'src/**/!(*.test).js']))
     .it('runs with files', (ctx) => {
-      expect(ctx.stdout).to.contain('@example/home');
-    });
+      expect(ctx.stdout).to.contain('@example/home')
+    })
 
   describe('errors', () => {
     test
@@ -19,13 +19,11 @@ describe('mfedeps', () => {
         cmd.run(['-d', 'non-existent-dir', '-f', 'src/**/!(*.test).js'])
       )
       .catch(hasErrorMessage)
-      .it('if --dir does not exist');
+      .it('if --dir does not exist')
 
     test
-      .do(() =>
-        cmd.run(['-d', 'mfe-deps-test-repo', '-f', 'src/**/*.not-match.js'])
-      )
+      .do(() => cmd.run(['-d', TEST_DIR, '-f', 'src/**/*.not-match.js']))
       .catch(hasErrorMessage)
-      .it(`if no --files match in dir`);
-  });
-});
+      .it(`if no --files match in dir`)
+  })
+})
