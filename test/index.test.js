c75d26ec77fdf400032e0f4225b9cbfe580637be
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-31 09:44:27 -0600

    Additional tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,20 @@
 const { expect, test } = require('@oclif/test')
 const cmd = require('../lib')
+const { join } = require('path')
 
-const hasErrorMessage = (error) => {
+const andValidateErrorIsThrown = (error) => {
   expect(error).to.be.ok
   expect(error.message).to.be.ok
 }
 
 const FIXTURE_DIR = 'mfe-dependencies-test-repo'
+
+/*
+It seems that mock-fs isn't compatible with oclif so I can't just mock these filesystem fixtures
+https://github.com/oclif/oclif/issues/182#issuecomment-433554720
+Instead, I'm going with real directory/file fixtures that will be maintained as a separate repo
+*/
+const fixtureAt = (fixtureDir) => join('./test/fixtures/', fixtureDir)
 
 describe('mfedeps', () => {
   test
@@ -21,12 +29,43 @@ describe('mfedeps', () => {
       .do(() =>
         cmd.run(['-d', 'non-existent-dir', '-f', 'src/**/!(*.test).js'])
       )
-      .catch(hasErrorMessage)
+      .catch(andValidateErrorIsThrown)
       .it('if --dir does not exist')
 
     test
       .do(() => cmd.run(['-d', FIXTURE_DIR, '-f', 'src/**/*.not-match.js']))
-      .catch(hasErrorMessage)
+      .catch(andValidateErrorIsThrown)
       .it(`if no --files match in dir`)
+
+    test
+      .do(() =>
+        cmd.run(['-d', fixtureAt('no-pkg-json'), '-f', 'src/**/!(*.test).js'])
+      )
+      .catch(andValidateErrorIsThrown)
+      .it(`if --dir does not have a package.json`)
+
+    test
+      .do(() =>
+        cmd.run([
+          '-d',
+          fixtureAt('empty-pkg-json'),
+          '-f',
+          'src/**/!(*.test).js',
+        ])
+      )
+      .catch(andValidateErrorIsThrown)
+      .it(`if package.json does not have a name field`)
+
+    test
+      .do(() =>
+        cmd.run([
+          '-d',
+          fixtureAt('invalid-pkg-json'),
+          '-f',
+          'src/**/!(*.test).js',
+        ])
+      )
+      .catch(andValidateErrorIsThrown)
+      .it(`if package.json cannot be parsed as JSON`)
   })
 })
