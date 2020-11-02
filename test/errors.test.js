4db215b52dffa17d5e5567c82b4be8988f9d20b3
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-02 12:40:03 -0700

    Update tests to validate correct error message

--- test/errors.test.js
+++ test/errors.test.js
@@ -1,45 +1,44 @@
 const { expect, test } = require('@oclif/test')
 const cmd = require('../lib')
 
-const andValidateErrorIsThrown = (error) => {
+const andValidateErrorMessage = (msg) => (error) => {
   expect(error).to.be.ok
-  expect(error.message).to.be.ok
+  expect(error.message).to.contain(msg)
 }
 
 describe('mfedeps errors', () => {
-  // The fixtures used in these tests are declared in setup-fs.js
-  test
-    .do(() =>
-      cmd.run(['-d', '/fixtures/non-existent-dir', '-f', 'src/**/!(*.test).js'])
-    )
-    .catch(andValidateErrorIsThrown)
-    .it('if --dir does not exist')
-
   test
     .do(() =>
       cmd.run(['-d', '/fixtures/no-files-match', '-f', 'src/**/*.not-match.js'])
     )
-    .catch(andValidateErrorIsThrown)
+    .catch(andValidateErrorMessage('No files match'))
     .it(`if no --files match in dir`)
+
+  test
+    .do(() =>
+      cmd.run(['-d', '/fixtures/non-existent-dir', '-f', 'src/**/!(*.test).js'])
+    )
+    .catch(andValidateErrorMessage('Unable to resolve dir'))
+    .it('if --dir does not exist')
 
   test
     .do(() =>
       cmd.run(['-d', '/fixtures/no-pkg-json', '-f', 'src/**/!(*.test).js'])
     )
-    .catch(andValidateErrorIsThrown)
+    .catch(andValidateErrorMessage('Unable to find package.json'))
     .it(`if --dir does not have a package.json`)
 
   test
     .do(() =>
       cmd.run(['-d', '/fixtures/empty-pkg-json', '-f', 'src/**/!(*.test).js'])
     )
-    .catch(andValidateErrorIsThrown)
+    .catch(andValidateErrorMessage('Unable to read package.json name field'))
     .it(`if package.json does not have a name field`)
 
   test
     .do(() =>
       cmd.run(['-d', '/fixtures/invalid-pkg-json', '-f', 'src/**/!(*.test).js'])
     )
-    .catch(andValidateErrorIsThrown)
+    .catch(andValidateErrorMessage('Unable to parse package.json'))
     .it(`if package.json cannot be parsed as JSON`)
 })
